11bc87a1d987d41c462ae038331985e4d3ffcb76
jiangyalin <jiangyl>
2023-08-08 15:16:34 +0800

    feat: 新增配置界面，参数解析，备注等

--- js/content-script.js
+++ js/content-script.js
@@ -25,7 +25,73 @@ const funDownload = (content, filename) => {
 
 const capitalizeFirstLetter = (str = '') => str.charAt(0).toUpperCase() + str.slice(1)
 
+const typeToDefaults = {
+  string: '\'\'',
+  integer: 0,
+  array: '[]',
+  number: 0,
+  boolean: false
+}
+
+const refToObj = (ref, schemas) => {
+  const data = []
+  const key = ref.substring(ref.lastIndexOf('/') + 1)
+  const properties = schemas[key].properties
+  for (const parameterKey in properties) {
+    const _ref = properties[parameterKey].$ref
+    if (_ref) {
+      const _key = _ref.substring(_ref.lastIndexOf('/') + 1)
+      const _description = (schemas[_key].description || '').replace(/&nbsp/g, '').replace(/<br \/>/g, '')
+
+      const isEnum = Boolean(schemas[_key].enum)
+      if (isEnum) {
+        data.push(`  ${parameterKey}: ${schemas[_key].enum[0]}, // ${_description}`)
+      }
+
+      const isObject = schemas[_key].type === 'object'
+      if (isObject) {
+        data.push(`  ${parameterKey}: { // ${_description}`)
+        data.push(...refToObj(_ref, schemas).map(item => '  ' + item))
+        data.push(`  },`)
+      }
+    } else {
+      data.push(`  ${parameterKey}: ${typeToDefaults[properties[parameterKey].type]}, // ${properties[parameterKey].description}`)
+    }
+  }
+  return data
+}
+
+const getParameter = (data, schemas) => {
+  const requestBodyArr = []
+  if (data.requestBody) {
+    const ref = data.requestBody.content['text/json']?.schema?.$ref || ''
+    if (ref) requestBodyArr.push(...refToObj(ref, schemas))
+  }
+
+  if (data.parameters) {
+    data.parameters.forEach(item => {
+      requestBodyArr.push(`  ${item.name}: ${typeToDefaults[item.schema.type]}, // ${item.description}`)
+    })
+  }
+  return requestBodyArr
+}
+
 let apiList = []
+
+let stencil = `/**
+ * @description <summary>
+ * @param {Object} data 请求参数
+ * @returns {Promise} ajax
+ */
+export const <method><apiName> = data => {
+  return axios({
+    url: '<path>',
+    method: '<method>',
+    data
+  })
+}`
+
+let configMap = {}
 
 const start = () => {
   const interval = setInterval(() => {
@@ -40,24 +106,44 @@ const start = () => {
           const id = 'operations-' + item.tags[0] + '-' + item.operationId
 
           const apiName = key.split('/').filter(item => item).map(item => capitalizeFirstLetter(item)).join('')
-          const code =
-            `
-// ${item.summary}
-export const ${method}${apiName} = data => {
-  return axios({
-    url: '${key}',
-    method: '${method}',
-    data
-  })
-}
-`
+          const requestBodyArr = getParameter(item, res.components.schemas)
+          const requestBody = '{\n' + requestBodyArr.join('\n') + '\n}'
+          const path = key
+          const summary = item.summary
+          const request = requestBody
+
+          const code = (configMap[window.location.host] || stencil).replace(/<summary>/g, summary)
+            .replace(/<method>/g, method)
+            .replace(/<apiName>/g, apiName)
+            .replace(/<path>/g, path)
+            .replace(/<request>/g, request)
+
+//           const code =
+//             `
+// /**
+//  * @description${summary}
+//  * @param {Object} data 请求参数
+//  * @returns {Promise} ajax
+//  */
+// export const ${method}${apiName} = data => {
+//   return axios({
+//     url: '${path}',
+//     method: '${method}',
+//     data
+//   })
+// }
+// `
 
           apiList.push({
             id,
+            apiName,
             tag: item.tags[0],
             method,
             code,
-            path: key
+            path,
+            requestBody,
+            request,
+            summary
           })
         }
       }
@@ -74,7 +160,11 @@ export const ${method}${apiName} = data => {
   }, 1000)
 }
 
+
 const init = () => {
+  chrome.storage.local.get('configMap', res => {
+    configMap = res.configMap || {}
+  })
   start()
 
   $('body').on('click', '.opblock-tag', function () {
@@ -85,10 +175,12 @@ const init = () => {
       apiList.filter(item => item.tag === tag).forEach(item => {
         const dom = $('#' + item.id + ' .opblock-summary')
         if (dom) dom.append('<button class="j-btn-copy" type="button" data-id="' + item.id +'">复制</button>')
+        if (dom) dom.append('<button class="j-btn-pass-parameters" type="button" data-id="' + item.id +'">传参</button>')
       })
     }, 300)
   })
 
+  // 复制
   $('body').on('click', '.j-btn-copy', function () {
     const api = apiList.find(item => item.id === $(this).attr('data-id'))
     copyToClipboard(api.code)
@@ -98,6 +190,17 @@ const init = () => {
     }, 600)
   })
 
+  // 传参
+  $('body').on('click', '.j-btn-pass-parameters', function () {
+    const api = apiList.find(item => item.id === $(this).attr('data-id'))
+    copyToClipboard(api.requestBody)
+    $(this).text('ok')
+    setTimeout(() => {
+      $(this).text('传参')
+    }, 600)
+  })
+
+  // 下载
   $('body').on('click', '.j-btn-down', function (event) {
     event.preventDefault()
     const tag = $(this).attr('data-tag')
--- js/popup.js
+++ js/popup.js
@@ -1,111 +1,64 @@
-// const dom = $('.j-list')
-// let listData = []
-//
-// // 复制
-// dom.on('click', '.j-copy', function () {
-//   const api = $(this).parents('.j-li').attr('data-api')
-//   const item = listData.find(item => item.url === api)
-//   navigator.clipboard.writeText(JSON.stringify(item.content, null, 2))
-//   $(this).text('ok')
-//   setTimeout(() => {
-//     $(this).text('复制')
-//   }, 600)
-// })
-//
-// // 下载
-// dom.on('click', '.j-down', function () {
-//   const api = $(this).parents('.j-li').attr('data-api')
-//   const fileName = api + '.json'
-//   const item = listData.find(item => item.url === api)
-//   funDownload(JSON.stringify(item, null, 2), fileName)
-// })
-//
-// // 设置过滤器
-// $('.j-filter-btn').click(function () {
-//   const value = $('.j-filter-it').val()
-//   chrome.storage.local.set({ filterUrl: value })
-//   $(this).text('ok')
-//   setTimeout(() => {
-//     $(this).text('确定')
-//   }, 600)
-// })
-//
-// // 刷新
-// $('.j-refresh').click(function () {
-//   init()
-//   $(this).text('ok')
-//   setTimeout(() => {
-//     $(this).text('刷新')
-//   }, 600)
-// })
-//
-// // 清空
-// $('.j-empty').click(function () {
-//   chrome.storage.local.set({ list: [] })
-//   init()
-//   $(this).text('ok')
-//   setTimeout(() => {
-//     $(this).text('清空')
-//   }, 600)
-// })
-//
-// window.onload = () => {
-//   init()
-// }
-//
-// // 初始化
-// const init = () => {
-//   chrome.storage.local.get('list', res => {
-//     listData = res.list
-//     const listDom = res.list.sort((a, b) => b.returnTime - a.returnTime).map(item => {
-//       return `<li class="u-li j-li" data-api="${item.url}" data-err="${item.content.code !== 200}">
-//                 <p class="u-name">/${item.url}</p>
-//                 <div class="u-btn-gp">
-//                   <button class="u-btn j-copy" type="button">复制</button>
-//                   <button class="u-btn j-down" type="button">下载</button>
-//                 </div>
-//               </li>`
-//     })
-//
-//     $('.j-list').html(listDom.join(''))
-//   })
-//
-//   try {
-//     chrome.storage.local.get('filterUrl', res => {
-//       $('.j-filter-it').val(res.filterUrl)
-//     })
-//   } catch (err) {
-//     $('.j-filter-it').val('http://192.168.3.104:9001')
-//   }
-// }
-//
-// const urlToObj = url => {
-//   const obj = {}
-//   const arr = url.split('?')
-//   obj.url = arr[0]
-//   obj.api = arr[0].split('/').filter((item, index) => index > 2).join('/')
-//   obj.params = arr[1] ? arr[1].split('&').map(item => {
-//     const _arr = item.split('=')
-//     return {
-//       key: _arr[0],
-//       value: _arr[1]
-//     }
-//   }) : []
-//   return obj
-// }
-//
-// // 生成下载文件并下载
-// const funDownload = (content, filename) => {
-//   // 创建隐藏的可下载链接
-//   const eleLink = document.createElement('a')
-//   eleLink.download = filename
-//   eleLink.style.display = 'none'
-//   // 字符内容转变成blob地址
-//   const blob = new Blob([content])
-//   eleLink.href = URL.createObjectURL(blob)
-//   // 触发点击
-//   document.body.appendChild(eleLink)
-//   eleLink.click()
-//   // 然后移除
-//   document.body.removeChild(eleLink)
-// }
+const defaultConfig = `/**
+ * @description <summary>
+ * @param {Object} data 请求参数
+ * @returns {Promise} ajax
+ */
+export const <method><apiName> = data => {
+  return axios({
+    url: '<path>',
+    method: '<method>',
+    data
+  })
+}`
+let url = ''
+
+// 保存
+$('.j-config-save').click(function () {
+  chrome.storage.local.get('configMap', res => {
+    const configMap = res.configMap || {}
+    configMap[url] = $('.j-config').val()
+    chrome.storage.local.set({ configMap: configMap })
+    $(this).text('ok')
+    setTimeout(() => {
+      $(this).text('保存')
+    }, 600)
+  })
+})
+
+// 重置
+$('.j-reset').click(function () {
+  $('.j-config').val(defaultConfig)
+  $(this).text('ok')
+  setTimeout(() => {
+    $(this).text('重置')
+  }, 600)
+})
+
+window.onload = () => {
+  init()
+}
+
+// 初始化
+const init = () => {
+  try {
+    chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+      const _url = new URL(tabs[0].url)
+      const domain = _url.hostname
+      const port = _url.port
+
+      url = domain + ':' + port
+
+      $('.j-name').text(url)
+      try {
+        chrome.storage.local.get('configMap', res => {
+          const config = (res.configMap || {})[url] || defaultConfig
+          $('.j-config').val(config)
+        })
+      } catch (err) {
+        $('.j-config').val(defaultConfig)
+      }
+    })
+  } catch (err) {
+    console.log('err', err)
+  }
+}
